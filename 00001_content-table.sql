a91f98e282c9a4783cc369b22e16aace45a4de99
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2018-01-09 20:48:16 -0600

    added the first migration for the basic tables

---
+++ 00001_content-table.sql
@@ -0,0 +1,29 @@
+-- +goose Up
+-- SQL in this section is executed when the migration is applied.
+
+CREATE TABLE namespace (
+    namespace_id SERIAL NOT NULL,
+    name text NOT NULL,
+    PRIMARY KEY(namespace_id)
+);
+
+CREATE TABLE content (
+    content_id SERIAL NOT NULL,
+    name text,
+    slug text NOT NULL,
+    created_by integer NOT NULL,
+    updated_by integer NOT NULL,
+    created_at timestamp with time zone DEFAULT NOW(),
+    updated_at timestamp with time zone DEFAULT NOW(),
+    content json NOT NULL,
+    namespace_id integer NOT NULL,
+    FOREIGN KEY (namespace_id) references namespace(namespace_id) ON DELETE cascade INITIALLY DEFERRED,
+    CONSTRAINT content_c1 UNIQUE (slug,namespace_id),
+    PRIMARY KEY (content_id)
+);
+
+-- +goose Down
+-- SQL in this section is executed when the migration is rolled back.
+
+DROP TABLE IF EXISTS content CASCADE;
+DROP TABLE IF EXISTS namespace CASCADE;
