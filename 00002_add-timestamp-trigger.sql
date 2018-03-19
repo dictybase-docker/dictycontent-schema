69dced576a5c6c4f8f9cd99a22679f7f079e8c87
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2018-03-19 14:13:24 -0500

    added timestamp trigger on update

---
+++ 00002_add-timestamp-trigger.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE OR REPLACE FUNCTION updated_at_column()	
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = now();
+    RETURN NEW;	
+END;
+$$ language 'plpgsql';
+
+CREATE TRIGGER updated_at_content
+    BEFORE UPDATE ON content
+    FOR EACH ROW EXECUTE PROCEDURE  updated_at_column();
+-- +goose StatementEnd
+-- +goose Down
+DROP FUNCTION IF EXISTS updated_at_column();
